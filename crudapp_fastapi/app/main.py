d58a8302189dfad3a0fe774922e1add919e541b7
Dipesh Kumar <dipeshkumar0853822@gmail.com>
2026-01-31 22:01:55 +0530

    starting SQLAlchemy

--- crudapp_fastapi/app/main.py
+++ crudapp_fastapi/app/main.py
@@ -97,28 +97,24 @@ def get_post(id:int, response: Response):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_404_NOT_FOUND)
 def delete_post(id: int):
-
     cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
     deleted_post = cursor.fetchone()
-    index = find_index_post(id)
-
-    if index == None:
+    conn.commit()
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
-    
-    my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, 
+    published = %s WHERE id = %s RETURNING *;""", (post.title, post.content, post.published, str(id)))
 
-    index = find_index_post(id)
+    updated_post = cursor.fetchone()
 
-    if index == None:
+    conn.commit()
+
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
-
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {"data": post_dict}
+    return {"data": updated_post}
