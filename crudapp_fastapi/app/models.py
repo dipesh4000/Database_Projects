e175d325c34d3bc213bec0d38d22d4e38d303907
Dipesh Kumar <dipeshkumar0853822@gmail.com>
2026-02-02 19:14:59 +0530

    fixed the URL encoding for the password

--- crudapp_fastapi/app/models.py
+++ crudapp_fastapi/app/models.py
@@ -12,4 +12,4 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean,server_default='True', nullable= False)
-    created_at = Column(TIMESTAMP(timezone=true)) 
+    created_at = Column(TIMESTAMP(timezone=True), nullable= False) 
