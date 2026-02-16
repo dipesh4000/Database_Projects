1bfaa112e7c3cc98f77ea22deba8fb1b54450cc7
Dipesh Kumar <dipeshkumar0853822@gmail.com>
2026-02-17 01:18:32 +0530

    added user id of the user who is posting

--- crudapp_fastapi/app/routers/post.py
+++ crudapp_fastapi/app/routers/post.py
@@ -44,7 +44,6 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
     # new_post = cursor.fetchone()
 
     # conn.commit()
-
     new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
